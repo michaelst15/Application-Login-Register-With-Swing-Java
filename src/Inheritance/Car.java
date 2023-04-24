7f7c4c3654056681a805695d376f1b0c76f1aae6
michaels <95168027+michaelst15@users.noreply.github.com>
2023-04-24 22:10:31 +0700

    Delete src/Inheritance directory

--- src/Inheritance/Car.java
+++
@@ -1,9 +0,0 @@
-package Inheritance;
-
-public class Car {
-    public String steering;
-    public String musicSystem;
-    public String airConditioner;
-    public String fridge;
-    public String entertaimentSystem;
-}
